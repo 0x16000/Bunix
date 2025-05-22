2944d84aca3c9089e003df2a4b5c6fc9f4358a82
0x16000 <openbsd.gnu@gmail.com>
2025-05-22 21:02:26 +0200

    Update version.h

--- include/version/version.h
+++ include/version/version.h
@@ -1,11 +1,11 @@
 #ifndef _BUNIX_VERSION_H
 #define _BUNIX_VERSION_H
 
-#define BUNIX_VERSION "0.0.8"
+#define BUNIX_VERSION "0.1.0"
 #define BUNIX_NAME "Bunix"
 #define BUNIX_HOSTNAME "root"
 #define BUNIX_ARCH "i386"
-#define BUNIX_BUILD_VERSION "0.0.7-20250418"
+#define BUNIX_BUILD_VERSION "0.1.0-20250522"
 #define BUNIX_OS "Bunix"
 
 #endif /* _BUNIX_VERSION_H */
